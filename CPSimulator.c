618281983cd0f4d7565a903a77fd96cf5d297edd
LAMP <anything>
2022-04-29 03:46:43 +0300

    Added comments

--- CPSimulator.c
+++ CPSimulator.c
@@ -9,33 +9,36 @@ pthread_mutex_t plk;
 Car **park;
 
 int main(int argc, char argv[]){
-	pthread_mutex_init(&plk, NULL);
+	pthread_mutex_init(&plk, NULL);		// used by G2DInit
 
 	// initialize the queue
 	Qinit(5);
 
 	// Initialize GUI
 	double n;
-	park = malloc(sizeof(Car)*PARK_SIZE);
-	G2DInit(park, PARK_SIZE, 5, 2, plk);
+	park = malloc(sizeof(Car)*PARK_SIZE);	// This will serve as park space
+	G2DInit(park, PARK_SIZE, IN_VALETS, OUT_VALETS, plk);	// initialize graphics
+	// Note, IN_VALETS and OUT_VALETS are the default values defined in CarPark.h
 	
+	// Create cars and show the graphics
 	usleep(5000);
-	int j = 1;
+	int j = 1; // Global cars counter. temporary, Fahad should take care of statistics later
 	while(true){
-		n = newCars(0.5);
+		n = newCars(0.5); // Get Poisson distributed number of cars (maybe 1, maybe 3, no one knows)
 		printf("Number of new cars: %f\n",n);
+		// Create n cars:
 		for(int i = 0; i<n; i++){
-			Car *temp = malloc(sizeof(Car));
-			CarInit(temp);		
-			updateStats(0, j++, 0, 0, 0, 0, 0, 0);
+			Car *temp = malloc(sizeof(Car));	// Allocate memory to Car pointer
+			CarInit(temp);				// Initialize car
+			updateStats(0, j++, 0, 0, 0, 0, 0, 0);	// temporary. Take care of all other stats later
 			printf("Car created, ID = %d\n", (*temp).cid);
 			if(QisFull()){
-				printf("Queue is full. Reject the car\n");
+				printf("Queue is full. Reject the car\n"); // Reject the car
 			}
-			else Qenqueue(temp);
+			else Qenqueue(temp);			// Enqueue the car
 		}
-		show();
-		while(getchar() != '\n');
+		show();						// Show the graphics
+		while(getchar() != '\n');			// wait for ENTER
 		//sleep(1);
 	}
 
