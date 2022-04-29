a715a24990316911e490d653a0fdb5d870f9e5b5
LAMP <anything>
2022-04-29 03:38:06 +0300

    Add git ignore and more cleanup on main file

--- CPSimulator.c
+++ CPSimulator.c
@@ -18,27 +18,7 @@ int main(int argc, char argv[]){
 	double n;
 	park = malloc(sizeof(Car)*PARK_SIZE);
 	G2DInit(park, PARK_SIZE, 5, 2, plk);
-/*
-	// Create i cars
-	int c = 5;
-	for(int i=0;i<c;){
-		Car *temp = malloc(sizeof(Car));
-		CarInit(temp);		
-		updateStats(0, ++i, 0, 0, 0, 0, 0, 0);
-		printf("Car created, ID = %d\n", (*temp).cid);
-		Qenqueue(temp);
-	}
-	Car *toPark = Qserve();
-	park[0] = toPark;
 	
-	// Testing Queue.c
-	printf("Peeked Car, ID = %d\n", Qpeek()->cid);
-	printf("Peeked Car, image = %s\n", Qpeek()->pn);
-	printf("Is empty?: %d, is full? %d, size: %d, capacity: %d\n",
-		QisEmpty(), QisFull(), Qsize(), Qcapacity());	
-*/
-	
-
 	usleep(5000);
 	int j = 1;
 	while(true){
